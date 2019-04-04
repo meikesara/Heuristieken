ccd780d36dddd1571eb226459c87302557c6f8bd
Nicole Jansen <nicole.jansen@student.uva.nl>
2019-04-04 12:07:20 +0200

    eerste opzet

---
+++ amino.py
@@ -0,0 +1,20 @@
+"""
+"""
+
+
+class Amino(object):
+    """
+    """
+
+    def __init__(self, id, type):
+        """
+        """
+        self.id = id
+        self.type = type
+        self.coordinate = []
+
+
+    def addCoordinate(self, coordinate):
+        """
+        """
+        self.coordinate = coordinate
---
+++ protein.py
@@ -0,0 +1,26 @@
+"""
+Class for protein.
+
+Meike, Janneke, Nicole
+"""
+
+from amino import Amino
+
+class Protein(object):
+    """
+    """
+
+    def __init__(self, proteinString):
+        """
+        """
+        self.proteinString = proteinString
+        self.aminoList = []
+
+
+    def createAminoList(self):
+        """
+        """
+        for i in self.proteinString:
+            self.aminoList.append(Amino(i, self.proteinString[i]))
+            if i == 0:
+                # coordinate should be (0,0)
