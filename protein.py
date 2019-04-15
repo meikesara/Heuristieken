0687ae377f99999c3eaa84e3b4c678f658031292
Nicole Jansen <nicole.jansen@student.uva.nl>
2019-04-15 12:31:30 +0200

    eerste opzet voor random plaatsing

--- protein.py
+++ protein.py
@@ -5,6 +5,7 @@ Meike, Janneke, Nicole
 """
 
 from amino import Amino
+import random
 
 class Protein(object):
     """
@@ -16,6 +17,7 @@ class Protein(object):
         self.proteinString = proteinString
         self.aminoList = []
         self.stability = []
+        self.occupied = []
 
 
     def createAminoList(self):
@@ -25,5 +27,26 @@ class Protein(object):
             self.aminoList.append(Amino(i, self.proteinString[i]))
             if i == 0 or i == 1:
                 self.aminoList[i].addCoordinate([0, i])
+                self.occupied.append([0, i])
+            else:
+                prevCo = self.aminoList[(i - 1)].getCoordinates()
+                posCo = []
+                posCo.append([(prevCo[0] - 1), prevCo[1]])
+                posCo.append([(prevCo[0] + 1), prevCo[1]])
+                posCo.append([prevCo[0], (prevCo[1] - 1)])
+                posCo.append([prevCo[0], (prevCo[1] + 1)])
+
+
+                print(posCo)
+                toRemove = []
+                for i in posCo:
+                    print(i)
+                    if i in self.occupied:
+                        toRemove.append(i)
+                for i in toRemove:
+                    if i in self.occupied:
+                        posCo.remove(i)
+                print(posCo)
+
 
                 # coordinate should be (0,0)
